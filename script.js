8c0655715293096355206825aa153c39a46e04ed
RapCodeurs <maureljoumessi9@gmail.com>
2024-02-29 23:44:29 +0100

    Exercice à faire: Afficher la date et l'heure sur un fond d'ecran au choix

---
+++ script.js
@@ -0,0 +1,23 @@
+function heureActuele() {
+  const date = new Date();
+
+  let day = date.toLocaleString("fr-FR", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  let heures = date.getHours();
+  const mediane = heures > 12 ? "PM" : "AM";
+
+  heures = heures % 12 || 12;
+  heures = heures.toString().padStart(2, 0);
+  let minutes = date.getMinutes().toString().padStart(2, 0);
+  let secondes = date.getSeconds().toString().padStart(2, 0);
+
+  const afficheheure = `${heures}:${minutes}:${secondes} ${mediane}`;
+  document.getElementById("heure").innerHTML = `${afficheheure}<br>${day}`;
+}
+
+heureActuele();
+setInterval(heureActuele, 1000);
